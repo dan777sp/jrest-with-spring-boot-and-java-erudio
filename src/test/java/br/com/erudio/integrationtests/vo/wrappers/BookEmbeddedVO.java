a97b9d1a974f716d4e79f98f86437b3ad575f9e8
dan777sp <danilon37@gmail.com>
2023-04-22 19:15:03 -0300

    first commit

---
+++ src/test/java/br/com/erudio/integrationtests/vo/wrappers/BookEmbeddedVO.java
@@ -0,0 +1,25 @@
+package br.com.erudio.integrationtests.vo.wrappers;
+
+import br.com.erudio.integrationtests.vo.BookVO;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class BookEmbeddedVO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @JsonProperty("bookVOList")
+    private List<BookVO> books;
+
+    public BookEmbeddedVO() {}
+
+    public List<BookVO> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<BookVO> books) {
+        this.books = books;
+    }
+}
